f125997cf08476085ae1f236ccebfb7399489384
Brayden Cleary <brayden.cleary@gmail.com>
2013-03-01 14:54:53 -0800

    Writing User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   has_many :responses, :through => :user_responses
   has_many :user_responses
 
+  validates :name, :email, :password_digest, :presence => true
+  validates :email, :format => { :with => /.*@.*\..*/}
+
   def surveys_created
     Survey.where(:creator_id => self.id)
   end
