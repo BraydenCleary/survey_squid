c4ffd980cd3a67e39de543ab081e8af192dda3b0
broppler <brooksmason@gmail.com>
2013-03-01 21:54:20 -0800

    css stylee

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,6 +3,8 @@ class Survey < ActiveRecord::Base
   has_many :user_surveys
   has_many :questions
 
+
+  validates :title, :uniqueness => true 
   def creator
     User.find(self.creator_id)
   end
--- public/javascript/responses.js
+++ public/javascript/responses.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
 
   $('#add_question').on('click', function(){
     displayCount++;
-    node = '<p><label for="Question'+displayCount+'">Question'+displayCount+': </label><input type="text" name="Question'+displayCount+'[question]" id="Question'+displayCount+'"><br><span id="add_response'+displayCount+'">Add Response</span>';
+    node = '<div class="question"><p><label for="Question'+displayCount+'">Question '+displayCount+' : </label><input type="text" name="Question'+displayCount+'[question]" id="Question'+displayCount+'"><br><span class="add" id="add_response'+displayCount+'">Add Response</span></div>';
     $('form').append(node);
     var Count = 0
 
     $('#add_response'+displayCount).on('click', function(){
     Count++;
-    node2 = '<p><label for="Response'+Count+'">Response'+Count+': </label><input type="text" name="Question'+displayCount+'[Response'+Count+']" id="Response'+Count+'">';
+    node2 = '<p><label class="responses" for="Response'+Count+'">Response '+Count+' : </label><input type="text" name="Question'+displayCount+'[Response'+Count+']" id="Response'+Count+'">';
     $(this).parent().append(node2);
   });
 
