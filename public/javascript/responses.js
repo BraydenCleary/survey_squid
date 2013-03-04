1d91ef7c1e7c227e158275cd9a26512c83dc437c
Apprentice <apprentice@devbootcamp.com>
2013-03-03 18:42:17 -0800

    fixed javascript bug, Updated css

--- public/javascript/responses.js
+++ public/javascript/responses.js
@@ -4,17 +4,19 @@ $(document).ready(function(){
   var create_entries = function(){
   
   var node = "";
-  var displayCount = 0  
+  var displayCount = 0
+    
 
   $('#add_question').on('click', function(){
     displayCount++;
     node = '<div class="question"><p><label for="Question'+displayCount+'">Question '+displayCount+' : </label><input type="text" name="Question'+displayCount+'[question]" id="Question'+displayCount+'"><br><span class="add" id="add_response'+displayCount+'">Add Response</span></div>';
     $('form').append(node);
     var Count = 0
-
+    
     $('#add_response'+displayCount).on('click', function(){
     Count++;
-    node2 = '<p><label class="responses" for="Response'+Count+'">Response '+Count+' : </label><input type="text" name="Question'+displayCount+'[Response'+Count+']" id="Response'+Count+'">';
+    var questionNum = $(this).attr('id').substr(12,14)
+    node2 = '<p><label class="responses" for="Response'+Count+'">Response '+Count+' : </label><input type="text" name="Question'+questionNum+'[Response'+Count+']" id="Response'+Count+'">';
     $(this).parent().append(node2);
   });
 
