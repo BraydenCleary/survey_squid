d4eea59848b1d09c066b07428e9d92a4f65b41c8
Ryan Moser <ryanpmoser@gmail.com>
2013-03-03 20:04:51 -0800

    refactor form creation js

--- public/javascript/responses.js
+++ public/javascript/responses.js
@@ -1,25 +1,20 @@
 $(document).ready(function(){
-  
-  var create_entries = function(){
-  
-  var node = "";
-  var displayCount = 0
+  var counter = 0;
+  var recounter = 0;
 
-  $('#add_question').on('click', function(){
-    displayCount++;
-    node = '<div class="question"><p><label for="Question'+displayCount+'">Question '+displayCount+' : </label><input type="text" name="Question'+displayCount+'[question]" id="Question'+displayCount+'"><br><span class="add" id="add_response'+displayCount+'">Add Response</span></div>';
-    $('form').append(node);
-    var Count = 0
-    
-    $('#add_response'+displayCount).on('click', function(){
-    Count++;
-    var questionNum = $(this).attr('id').substr(12,14)
-    node2 = '<p><label class="responses" for="Response'+Count+'">Response '+Count+' : </label><input type="text" name="Question'+questionNum+'[Response'+Count+']" id="Response'+Count+'">';
-    $(this).parent().append(node2);
+  $(".add").on("click", "p", function(e){
+    console.log($(this).attr("question-id"));
+    $(this).append("<br/><input type='text' name='q"+$(this).attr('question-id')+'r'+recounter+"' placeholder='response'>");
+    recounter++;
+    return false;
   });
 
-
+  $(".add").on("click", "input", function(e){
+    return false;
   });
-  }
-  create_entries();
+
+  $(".add").click(function(e){
+    $(this).append("<br/>Question<br/><input type='text' name='q" + counter+"'' placeholder='question'><p question-id="+counter+">responses</p>");
+    counter++;
+  });
 });
